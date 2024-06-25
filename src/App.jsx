08202b123d95230f36555b5c393d0ae86555e237
Rahul suthar <sutharrahul119@gmail.com>
2024-06-25 14:31:03 +0530

    app created

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="px-8">
+      <Button>hello</Button>
+    </div>
+  );
+}
+
+export default App;
