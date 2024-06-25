1a369c834913e9d152dbf9fca9ca40b8623f07ab
Rahul suthar <sutharrahul119@gmail.com>
2024-06-25 15:36:31 +0530

    env file setUp

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Button } from "./components/ui/button";
 
 function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
   return (
     <div className="px-8">
       <Button>hello</Button>
