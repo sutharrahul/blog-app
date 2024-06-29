a50087a81b9759bb1c8cbd0f50ad7d515f65f9b4
Rahul suthar <sutharrahul119@gmail.com>
2024-06-29 17:10:08 +0530

    singup and login page create

--- src/components/Input.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useId } from "react";
-
-function Input() {
-  const id = useId();
-  return <div>Input</div>;
-}
-
-export default Input;
--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Footer from "./Footer/Footer";
 import Container from "./Container/Container";
 import LogoutBtn from "./Header/LogoutBtn";
 import Logo from "./Logo";
+import { Button } from "./ui/button";
 
-export { Header, Footer, Container, LogoutBtn, Logo };
+export { Header, Footer, Container, LogoutBtn, Logo, Button };
