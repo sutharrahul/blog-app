009a6098d1bedb7064240be75a105ae6bebde3ac
Rahul suthar <sutharrahul119@gmail.com>
2024-06-29 18:11:44 +0530

    post-form create

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,18 @@ import Container from "./Container/Container";
 import LogoutBtn from "./Header/LogoutBtn";
 import Logo from "./Logo";
 import { Button } from "./ui/button";
+import Input from "postcss/lib/input";
+import Select from "./Select";
+import RTE from "./RTE";
 
-export { Header, Footer, Container, LogoutBtn, Logo, Button };
+export {
+  Header,
+  Footer,
+  Container,
+  LogoutBtn,
+  Logo,
+  Button,
+  Input,
+  Select,
+  RTE,
+};
